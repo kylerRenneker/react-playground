b3e3ed6eaa96762f9e8c96b1867fccc767e55edb
Kyler <Krenneker16@gmail.com>
2019-03-18 19:57:35 -0400

    initial commit for react-playground

---
+++ src/state/TheDate.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class TheDate extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return <div />
+    }
+}
+
+export default TheDate
